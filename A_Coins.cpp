c11b1f76e02043fa4686de1fd29c40b7143db93c
Astroxphiliauxx <astroxphiliauxx@gmail.com>
2024-08-08 02:03:58 +0530

    yay

---
+++ A_Coins.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+
+#define ll long long
+#define eb emplace_back
+#define fast_io ios_base::sync_with_stdio(false);
+#define loop(i, a, b) for(int i = a; i < b; i++)
+#define loop(n) for(int i = 0; i < n; i++)
+#define mod 1000000007
+#define all(x) (x).begin(), (x).end()
+#define yes cout << "yes"
+#define no cout << "no"
+
+using namespace std;
+
+void chalna_to_chahiye() {
+
+ll n,k;
+cin>>n>>k;
+
+if(n & 1){
+    if(((n%k) & 1)&& k%2==0){
+        cout<<"No"<<endl;
+    }
+    else{
+        cout<<"Yes"<<endl;
+    }
+}
+else cout<<"Yes"<<endl;
+
+}
+
+int main() {
+    fast_io;
+    cout << fixed;
+    cout << setprecision(10);
+
+    int t = 1;
+    cin >> t;
+    while(t--) {
+        chalna_to_chahiye();
+    }
+}
